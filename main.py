48c08d699e46c271f582b087ff1ce2adb018c11a
JustinInthaly21 <825134@mystma.org>
2021-02-02 17:36:36 +0000

    counta 2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def countA(w):
     t = 0
-    for i in range(len(w)):
-        if i == "a":
-            t = t + 1
+    for i in range(0, len(w)):
+        t = t + i
+        if w[i] == "a":
+            return t + 1
+         
 
-print(countA("mac"))
+print(countA("macaaa"))
